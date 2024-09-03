8e7dd0cfb50b253c9c9fe5bd266090e3f8162830
Benjdiya <walid.benjdiya10@gmail.com>
2024-09-03 19:23:34 +0100

    F1.5.10

--- src/pages/LastStep.jsx
+++ src/pages/LastStep.jsx
@@ -50,27 +50,27 @@ const LastStep = () => {
     }
     useEffect(() => {
         const numOffers = 9; // Nombre maximum d'offres à afficher
-    
+
         // Effectuer une requête GET pour récupérer les offres
         fetchJsonp('https://d1s282k6wlfspe.cloudfront.net/public/offers/feed.php?user_id=626243&api_key=47d1f1da55272db2e62d30e041449eb7&s1=&s2=', {
             method: "GET",
             headers: {
-              'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             },
-          })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log('data');
-            console.log(data);
-            // Limiter le nombre d'offres affichées
-            const trimmedOffers = data.splice(9, 20);
-            setOffers(trimmedOffers);
-          })
-          .catch((error) => {
-            console.error('Error fetching offers:', error);
-          });
-      }, []);
-    
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('data');
+                console.log(data);
+                // Limiter le nombre d'offres affichées
+                const trimmedOffers = data.splice(9, 20);
+                setOffers(trimmedOffers);
+            })
+            .catch((error) => {
+                console.error('Error fetching offers:', error);
+            });
+    }, []);
+
 
     useEffect(() => {
         const storedData = localStorage.getItem('data');
@@ -129,77 +129,91 @@ const LastStep = () => {
                         gap: '16px' // Espacement entre les cartes
                     }}
                 >
-                    {offers.sort(() => 0.5 - Math.random()).slice(0, 9).map((item, index) => (
-                        <Card className="hover-image"
-                            onClick={() => window.open(`${item.url}${localStorage.getItem('data')}`, '_blank')}
-                            key={index}
-                            sx={{
-                                position: 'relative', // Nécessaire pour positionner l'image de survol
-                                maxWidth: 'calc(120px - 66px)', // 5 cartes par ligne
-                                minWidth: 'calc(32% - 16px)',
-                                boxSizing: 'border-box', // S'assure que le padding est inclus dans la largeur totale
-                                overflow: 'hidden', // Assure que l'image de survol ne dépasse pas les limites de la carte
+                    {offers.length > 0 ? (
+                        offers.map((item, index) => (
+                            <Card className="hover-image"
+                                onClick={() => window.open(`${item.url}${localStorage.getItem('data')}`, '_blank')}
+                                key={index}
+                                sx={{
+                                    position: 'relative', // Nécessaire pour positionner l'image de survol
+                                    maxWidth: 'calc(120px - 66px)', // 5 cartes par ligne
+                                    minWidth: 'calc(32% - 16px)',
+                                    boxSizing: 'border-box', // S'assure que le padding est inclus dans la largeur totale
+                                    overflow: 'hidden', // Assure que l'image de survol ne dépasse pas les limites de la carte
 
+                                }}
+                            >
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        image="/cadeau.png"
+                                        alt="green iguana"
+                                        sx={{ width: '100%', height: 'auto' }} // Assurez-vous que l'image s'adapte correctement
+                                    />
+                                    <CardContent sx={{ margin: '0px', padding: '14px', justifyContent: 'center', textAlign: 'center' }}>
+                                        <Typography
+                                            gutterBottom
+                                            sx={{
+                                                fontWeight: 'bold',
+                                                fontSize: '1.6vw', // Utilisez des unités relatives pour une meilleure adaptation
+                                                display: 'flex',
+                                                overflow: 'auto',
+                                                justifyContent: 'center',
+                                                marginBottom: '8px', // Ajoutez un espace entre les lignes si nécessaire
+                                            }}
+                                            component="div"
+                                        >
+                                            {itemName.toUpperCase()} Gift
+                                        </Typography>
+                                        <span
+                                            style={{
+                                                color: '#b9babc',
+                                                fontSize: '0.75rem', // Utilisez des unités relatives pour plus de flexibilité
+                                                display: 'block', // Utilisez block pour s'assurer que le texte s'affiche correctement
+                                                margin: '0 auto', // Centrez le texte horizontalement
+                                            }}
+                                        >
+                                            TOKE ME NOW
+                                        </span>
+                                        {/* Image à afficher lors du survol */}
+                                        <Box
+                                            component="img"
+                                            src={`/${itemName}C.png`} // Remplacez par le chemin vers l'image à afficher
+                                            alt="Hover"
+                                            sx={{
+                                                position: 'absolute',
+                                                top: '0',
+                                                left: '0',
+                                                width: '100%',
+                                                height: '100%',
+                                                objectFit: 'cover',
+                                                opacity: '0',
+                                                '&:hover': {
+                                                    opacity: '1',
+                                                },
+                                                transition: 'opacity 0.3s ease-in-out',
+                                                zIndex: 0,
+                                            }}
+                                        // className="hoverImage"
+                                        />
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+
+                        ))
+                    ) : (
+                        <Typography
+                            sx={{
+                                textAlign: 'center',
+                                marginTop: '20px',
+                                fontWeight: 'bold',
+                                fontSize: '1.5rem',
+                                color: '#ff5722',
                             }}
                         >
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    image="/cadeau.png"
-                                    alt="green iguana"
-                                    sx={{ width: '100%', height: 'auto' }} // Assurez-vous que l'image s'adapte correctement
-                                />
-                                <CardContent sx={{ margin: '0px', padding: '14px', justifyContent: 'center', textAlign: 'center' }}>
-                                    <Typography
-                                        gutterBottom
-                                        sx={{
-                                            fontWeight: 'bold',
-                                            fontSize: '1.6vw', // Utilisez des unités relatives pour une meilleure adaptation
-                                            display: 'flex',
-                                            overflow: 'auto',
-                                            justifyContent: 'center',
-                                            marginBottom: '8px', // Ajoutez un espace entre les lignes si nécessaire
-                                        }}
-                                        component="div"
-                                    >
-                                        {itemName.toUpperCase()} Gift
-                                    </Typography>
-                                    <span
-                                        style={{
-                                            color: '#b9babc',
-                                            fontSize: '0.75rem', // Utilisez des unités relatives pour plus de flexibilité
-                                            display: 'block', // Utilisez block pour s'assurer que le texte s'affiche correctement
-                                            margin: '0 auto', // Centrez le texte horizontalement
-                                        }}
-                                    >
-                                        TOKE ME NOW
-                                    </span>
-                                    {/* Image à afficher lors du survol */}
-                                    <Box
-                                        component="img"
-                                        src={`/${itemName}C.png`} // Remplacez par le chemin vers l'image à afficher
-                                        alt="Hover"
-                                        sx={{
-                                            position: 'absolute',
-                                            top: '0',
-                                            left: '0',
-                                            width: '100%',
-                                            height: '100%',
-                                            objectFit: 'cover',
-                                            opacity: '0',
-                                            '&:hover': {
-                                                opacity: '1',
-                                            },
-                                            transition: 'opacity 0.3s ease-in-out',
-                                            zIndex: 0,
-                                        }}
-                                    // className="hoverImage"
-                                    />
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-
-                    ))}
+                            Unfortunately, your country not supported
+                        </Typography>
+                    )}
                 </Stack>
 
 
