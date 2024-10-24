a33d12407e2a4c4d664fffa042582ce6bcc04ba9
Benjdiya <walid.benjdiya10@gmail.com>
2024-10-24 17:19:43 +0100

    thankyouV2

--- src/pages/AzureRessourceDone.jsx
+++ src/pages/AzureRessourceDone.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const AzureRessourceDone = () => {
     return (
         <div>
-            Notre service est actuellement en maintenance et sera de nouveau disponible très prochainement. Merci de votre patience et de votre compréhension.
+            <h3 style={{margin:'15px'}}>
+                Notre service est actuellement en maintenance et sera de nouveau disponible très prochainement. Merci de votre patience et de votre compréhension.
+            </h3>
         </div>
     );
 }
