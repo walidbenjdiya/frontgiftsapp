825b56b5a637bea5ee803a93964b2520ddef988d
Benjdiya <walid.benjdiya10@gmail.com>
2025-01-11 15:28:29 +0100

    11/01/25.3

--- src/pages/AzureRessourceDone.jsx
+++ src/pages/AzureRessourceDone.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const AzureRessourceDone = () => {
     return (
         <div>
-            <h1>Coach Oussama el Abbassi </h1>
-            <img src='/oussama.jpeg'></img>
+            {/* <h1>Coach Oussama el Abbassi </h1>
+            <img src='/oussama.jpeg'></img> */}
             <h3 style={{margin:'15px'}}>
                 Notre service est actuellement en maintenance et sera de nouveau disponible très prochainement. Merci de votre patience et de votre compréhension.
             </h3>
