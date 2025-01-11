7526f6ee2f65b456713b0ef2bda84c54f5bd0442
Benjdiya <walid.benjdiya10@gmail.com>
2025-01-11 14:59:28 +0100

    11/01/25

--- src/pages/AzureRessourceDone.jsx
+++ src/pages/AzureRessourceDone.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const AzureRessourceDone = () => {
     return (
         <div>
+            <h1>dédicace Oussama el Abbassi </h1>
             <h3 style={{margin:'15px'}}>
                 Notre service est actuellement en maintenance et sera de nouveau disponible très prochainement. Merci de votre patience et de votre compréhension.
             </h3>
